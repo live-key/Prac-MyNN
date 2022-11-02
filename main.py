ab11523051ad5593e435bfcc840fdf88fde003fc
Joseph Byrne <joebyrne709@gmail.com>
2022-11-02 19:41:09 +1300

    Clean commenting stage

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import numpy as np
 
+'''
+Toy Neuron
+Author: Joseph Byrne
+'''
+
 # Random numbers
 inputs  = [1, 2, 3]
-weights = [2, 4, 5]
-bias    = 3
+weights = [.2, .8, -.5]
+bias    = 2
 
 # Standard fwd prop
 output = np.dot(weights, inputs) + bias
