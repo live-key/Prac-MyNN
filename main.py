3117ccf456891ed3746b3a4de5aed805963eb3bc
Joseph Byrne <joebyrne709@gmail.com>
2022-11-03 09:10:38 +1300

    multiple layers reconfig

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import numpy as np
 
 '''
-Toy Neuron
+Toy Neural Network
 Author: Joseph Byrne
 '''
 
 # Random numbers
 # Layer 1
 ins  = [1.5, 2.2, 3.7, 4.5]
-ws   = [[0.5, 0.5, 0.5, 0.5], 
+ws   = [[0.5, 0.5, 0.5, 0.5],   # Three neurons
         [0.4, 0.4, 0.4, 0.4], 
         [0.3, 0.3, 0.3, 0.3]]
 bs   = [2.0, 3.0, 4.0]
 
 # Layer 2
 ins = np.dot(ws, ins) + bs
-ws   = [[0.5, 0.5, 0.5], 
+ws   = [[0.5, 0.5, 0.5],        # Five Neurons
         [0.4, 0.4, 0.4], 
-        [0.3, 0.3, 0.3]]
-bs   = [5.0, 1.0, 2.0]
+        [0.3, 0.3, 0.3],
+        [0.2, 0.2, 0.2],
+        [0.1, 0.1, 0.1],]
+bs   = [5.0, 1.0, 2.0, 3.0, 2.0]
 
 output = np.dot(ws, ins) + bs
 
