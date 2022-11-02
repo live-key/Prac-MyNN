104f69fdc3588938fb47c46be2dacd55d2b89f05
Joseph Byrne <joebyrne709@gmail.com>
2022-11-03 09:07:03 +1300

    multiple layers

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ Author: Joseph Byrne
 '''
 
 # Random numbers
-inputs  = [1, 2, 3]
-weights = [.2, .8, -.5]
-bias    = 2
+# Layer 1
+ins  = [1.5, 2.2, 3.7, 4.5]
+ws   = [[0.5, 0.5, 0.5, 0.5], 
+        [0.4, 0.4, 0.4, 0.4], 
+        [0.3, 0.3, 0.3, 0.3]]
+bs   = [2.0, 3.0, 4.0]
 
-# Standard fwd prop
-output = np.dot(weights, inputs) + bias
+# Layer 2
+ins = np.dot(ws, ins) + bs
+ws   = [[0.5, 0.5, 0.5], 
+        [0.4, 0.4, 0.4], 
+        [0.3, 0.3, 0.3]]
+bs   = [5.0, 1.0, 2.0]
+
+output = np.dot(ws, ins) + bs
+
 print(output)
