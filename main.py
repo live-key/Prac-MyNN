0912b125ffd88a43bd7344183cb3097c9322a020
Joseph Byrne <joebyrne709@gmail.com>
2022-11-03 09:45:04 +1300

    Layer class implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from layer import Layer
 
 '''
 Toy Neural Network
@@ -9,22 +10,12 @@ Author: Joseph Byrne
 # Input Layer
 ins  = [[ 1.0,  2.0,  3.0,  2.5],
         [ 2.0,  5.0, -1.0,  2.0],
-        [-1.5,  2.7,  3.3,  -0.8]]
+        [-1.5,  2.7,  3.3, -0.8]]
 
-ws   = [[ 0.20,  0.80, -0.50,  1.00],
-        [ 0.50, -0.91,  0.26, -0.50], 
-        [-0.26, -0.27,  0.17,  0.87]]
+layer1 = Layer(4,5)
+layer2 = Layer(5,2)
 
-bs   = [2.0, 3.0, 0.5]
+layer1.forward(ins)
+layer2.forward(layer1.res)
 
-out1 = np.dot(ins, np.transpose(ws)) + bs
-
-ws   = [[ 0.10, -0.14,  0.50],
-        [-0.50,  0.12, -0.33], 
-        [-0.44,  0.73, -0.13]]
-
-bs   = [-1.0, 2.0, -0.5]
-
-out2 = np.dot(out1, np.transpose(ws)) + bs
-
-print(out2)
+print(layer2.res)
