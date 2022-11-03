ce4b0a97a34ff35a8c01a28560de8823c060b136
Joseph Byrne <joebyrne709@gmail.com>
2022-11-04 10:59:09 +1300

    SoftMax activation, first neural net run

--- activation.py
+++ activation.py
@@ -4,16 +4,21 @@ def ReLU(self, ins):
     self.res = np.maximum(0, ins)
 
 def SoftMax(self, ins):
-    self.res = np.maximum(0, ins)
+    # Overflow protection
+    ins -= np.max(ins, axis=1, keepdims=True)
+
+    # Exponentiation
+    exp_val = np.exp(ins)
+    self.res = exp_val / np.sum(exp_val, axis=1, keepdims=True)
 
 class Activation:
     def __init__(self, type):
         self.type = type.upper()
 
     def activate(self, ins):
-        self.activationDict = {
-            "RELU":  ReLU
+        self.activation_dict = {
+            "RELU":  ReLU,
             "SOFT":  SoftMax
         }
-        self.activationDict[self.type](self, ins)
+        self.activation_dict[self.type](self, ins)
         
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import numpy as np
+
 from layer import Layer
 from activation import Activation
+
 import nnfs
 from nnfs.datasets import spiral_data
-
 nnfs.init()
 
-X, y = spiral_data(100, 3)
+# Input layer
+X, y = spiral_data(samples=100, classes=3)
 
 '''
 Toy Neural Network
 Author: Joseph Byrne
 '''
 
-# Input Layer
-
-layer1 = Layer(n_ins=2, n_neurons=5)
+# Hidden layer 1
+layer1 = Layer(n_ins=2, n_neurons=3)
 activation1 = Activation(type="relu")
 
+# Hidden layer 2
+layer2 = Layer(n_ins=3, n_neurons=3)
+activation2 = Activation(type="soft")
+
+# Fwd prop
 layer1.forward(ins=X)
 activation1.activate(ins=layer1.res)
+
+layer2.forward(ins=activation1.res)
+activation2.activate(layer2.res)
+
+# Output layer
+print(activation2.res[:5])
