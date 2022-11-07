6aae7b49cc8880b46a803e217349fb8f00df066b
Joseph Byrne <joebyrne709@gmail.com>
2022-11-08 12:58:43 +1300

    add SMLoss combo file

---
+++ sm_loss.py
@@ -0,0 +1,23 @@
+import numpy as np
+from activation import Activation
+from loss import Loss, Loss_CCE
+
+class SMLoss:
+    def __init__(self):
+        self.activation = Activation("soft")
+        self.loss = Loss_CCE()
+
+    def forward(self, inputs, y_true):
+        self.activation.activate(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+
+    def backward(self, d_vals, y_true):
+        samples = len(d_vals)
+
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        self.del_inputs = d_vals.copy()
+        self.del_inputs[range(samples), y_true] -= 1
+        self.del_inputs = self.del_inputs / samples
